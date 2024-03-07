cc88804585df0e82447f093742a117ee60960aa4
Yeonghyeon Kim <hyxxnii@naver.com>
2024-03-07 14:13:52 +0900

    Create [실버 4] 1026.py

---
+++ 백준-온라인-저지/그리디/[실버 4] 1026.py
@@ -0,0 +1,9 @@
+# https://www.acmicpc.net/problem/1026
+
+n = int(input())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+
+A.sort()
+B.sort(reverse=True)
+print(sum(a*b for a, b in zip(A, B)))
