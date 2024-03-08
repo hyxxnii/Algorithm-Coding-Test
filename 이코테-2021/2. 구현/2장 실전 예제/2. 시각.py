b0004d4462b09d6f671d5a3bc0e836e7e20dc7e8
Yeonghyeon Kim <hyxxnii@naver.com>
2024-03-08 11:54:14 +0900

    Create 2. 시각.py

---
+++ 이코테-2021/2. 구현/2장 실전 예제/2. 시각.py
@@ -0,0 +1,11 @@
+n = int(input())
+cnt = 0
+
+for h in range(n+1):
+    for m in range(60):
+        for s in range(60):
+            t = str(h) + str(m) + str(s)
+            if '3' in t:
+                cnt += 1
+
+print(cnt)
