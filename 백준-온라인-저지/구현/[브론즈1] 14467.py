d04703f617c2810b89d62b1f33b036fd63833639
Yeonghyeon Kim <hyxxnii@naver.com>
2024-03-09 13:39:19 +0900

    Create [브론즈1] 14467.py

---
+++ 백준-온라인-저지/구현/[브론즈1] 14467.py
@@ -0,0 +1,16 @@
+n = int(input())
+
+ob = [-1] * 10
+cnt = 0
+
+for _ in range(n):
+    a, b = map(int, input().split())
+    
+    if ob[a-1] == -1:
+        ob[a-1] = b
+        
+    if ob[a-1] != b:
+        ob[a-1] = b
+        cnt += 1
+    
+print(cnt)
