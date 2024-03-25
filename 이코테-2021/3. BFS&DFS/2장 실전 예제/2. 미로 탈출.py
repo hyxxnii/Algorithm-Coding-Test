672f9170c99b8e9e5bdbdfc244840919da9a2b3b
Yeonghyeon Kim <hyxxnii@naver.com>
2024-03-25 15:46:13 +0900

    Create 2. 미로 탈출.py

---
+++ 이코테-2021/3. BFS&DFS/2장 실전 예제/2. 미로 탈출.py
@@ -0,0 +1,31 @@
+import sys
+from collections import deque
+
+n, m = map(int, sys.stdin.readline().split())
+graph = []
+for _ in range(n):
+    graph.append(list(map(int, sys.stdin.readline().strip())))
+
+dx = [-1, 1, 0, 0] # 상하좌우
+dy = [0, 0, -1, 1]
+
+def bfs(x, y):
+    q = deque()
+    q.append((x, y))
+    while q:
+        x, y = q.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            
+            if nx < 0 or nx >= n or ny < 0 or ny >= m:
+                continue
+            if graph[nx][ny] == 0:
+                continue
+            if graph[nx][ny] == 1:
+                graph[nx][ny] = graph[x][y] + 1
+                q.append((nx, ny))
+    
+    return graph[n-1][m-1]
+
+print(bfs(0, 0))
