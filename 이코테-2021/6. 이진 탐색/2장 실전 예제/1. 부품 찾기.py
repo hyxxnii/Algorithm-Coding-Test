771b48c8fd7e8364a4f5f318d3cd61faa0981d8c
Yeonghyeon Kim <hyxxnii@naver.com>
2024-03-28 15:19:28 +0900

    Update 1. 부품 찾기.py

--- 이코테-2021/6. 이진 탐색/2장 실전 예제/1. 부품 찾기.py
+++ 이코테-2021/6. 이진 탐색/2장 실전 예제/1. 부품 찾기.py
@@ -23,3 +23,6 @@ parts.sort()
 for i in finds:
     result = binary_search(parts, i, 0, n-1)
     print(result, end=' ')
+    
+### 또는 단순히 set() 함수를 사용하여 가게 내 유니크한 부품들을 따로 저장해두고
+### 찾고자하는 부품이 있는지 없는지 for문으로 연산
