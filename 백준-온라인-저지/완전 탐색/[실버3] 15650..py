d1518b492d1e73406821ca3ec84c134f5704a453
Yeonghyeon Kim <hyxxnii@naver.com>
2024-04-04 12:43:47 +0900

    Create [실버3] 15650..py

---
+++ 백준-온라인-저지/완전 탐색/[실버3] 15650..py
@@ -0,0 +1,12 @@
+# https://www.acmicpc.net/problem/15650
+
+import sys
+from itertools import combinations
+
+
+n, m = map(int, input().split())
+data = list(range(1, n+1))
+result = list(combinations(data, m))
+
+for i in result:
+    print(*i)
