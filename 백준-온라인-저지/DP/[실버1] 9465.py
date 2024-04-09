e1d8bae38780723e9a1797f83cd1c922d25eee30
Yeonghyeon Kim <hyxxnii@naver.com>
2024-04-09 21:19:50 +0900

    Update [실버1] 9465.py

--- 백준-온라인-저지/DP/[실버1] 9465.py
+++ 백준-온라인-저지/DP/[실버1] 9465.py
@@ -12,11 +12,17 @@ for _ in range(T):
     
     dp[0][0] = arr[0][0]
     dp[1][0] = arr[1][0]
+    if n == 1:
+        print(max(dp[0][n-1], dp[1][n-1]))
+        continue
+        
     dp[0][1] = arr[1][0] + arr[0][1]
     dp[1][1] = arr[0][0] + arr[1][1]
+    if n == 2:
+        print(max(dp[0][n-1], dp[1][n-1]))
+        continue
     
     for i in range(2, n):
         dp[0][i] = max(dp[1][i-1], dp[1][i-2]) + arr[0][i]
         dp[1][i] = max(dp[0][i-1], dp[0][i-2]) + arr[1][i]
-    
     print(max(dp[0][n-1], dp[1][n-1]))
