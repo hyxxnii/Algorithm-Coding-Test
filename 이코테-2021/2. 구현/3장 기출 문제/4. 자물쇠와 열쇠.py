0e8249db76c9a6f57c6b5c45a799366ec29a0509
Yeonghyeon Kim <hyxxnii@naver.com>
2024-04-18 16:28:15 +0900

    Update 4. 자물쇠와 열쇠.py

--- 이코테-2021/2. 구현/3장 기출 문제/4. 자물쇠와 열쇠.py
+++ 이코테-2021/2. 구현/3장 기출 문제/4. 자물쇠와 열쇠.py
@@ -12,7 +12,7 @@ def rotate_90_degree(mat):
 
 def check_lock(lock):
     # 자물쇠 중간 부분이 모두 1인지 확인
-    true_n = len(lock) // 3
+    n = len(lock) // 3
     for x in range(n):
         for y in range(n):
             if not lock[n+x][n+y]:
@@ -22,29 +22,28 @@ def check_lock(lock):
 def solution(key, lock):
     n = len(lock)
     m = len(key)
-    
     # 자물쇠 크기 3배 늘리기
     large_lock = [[0] * (n*3) for _ in range(n*3)]
-    for i in range(n): # 자물쇠 중앙에 원래 행렬 값 입력
+    # 자물쇠 중앙에 원래 행렬 값 입력
+    for i in range(n):
         for j in range(n):
             large_lock[n+i][n+j] = lock[i][j]
     
     for _ in range(4): # 90도씩 회전
-        new_key = rotate_90_degree(key)
+        key = rotate_90_degree(key)
         for x in range(n*3 - m + 1): # 행
             for y in range(n*3 - m + 1): # 열
                 # 자물쇠에 열쇠 끼워 넣기
                 for i in range(m):
                     for j in range(m):
-                        large_lock[x+i][y+j] += new_key[i][j]
-                if check_lock:
+                        large_lock[x+i][y+j] += key[i][j]
+                if check_lock(large_lock):
                     return True
                 
                 # 자물쇠에서 열쇠 다시 빼기
                 for i in range(m):
                     for j in range(m):
-                        large_lock[x+i][y+j] -= new_key[i][j]
-                
+                        large_lock[x+i][y+j] -= key[i][j]
     return False
 
 key = [[0, 0, 0], [1, 0, 0], [0, 1, 1]]
